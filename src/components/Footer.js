78634ee0e50fa9c5fdf1d6f515482f7d058e9cb5
Md Arifur Rahman <rahman1193arif@gmail.com>
2022-03-06 16:03:19 +0600

    start

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/1200px-MasterCard_Logo.svg.png"
           />
         </div> */}
-        <div className="card-name">
+        {/* <div className="card-name">
           <img
             alt="visa"
             src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png"
           />
-        </div>
+        </div> */}
         <div className="card-name">
           <img
             alt="paypal"
